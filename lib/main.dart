2933526ec88c8227167c903c1ee1f6bccec59cb0
RoRaut2001 <rohinibraut20@gmail.com>
2024-08-29 11:13:34 +0530

    Remove conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,12 @@
-import 'package:bright_weddings/Component/footer.dart';
-import 'package:bright_weddings/View/Dashboard/dashboard.dart';
 import 'package:bright_weddings/View/EditProfile/editprofile.dart';
-import 'package:bright_weddings/View/Login/login.dart';
-import 'package:bright_weddings/View/ProfileSetting/profilesetting.dart';
-import 'package:bright_weddings/chatlists/chatlistmobile.dart';
-import 'package:bright_weddings/chatlists/chatlistweb.dart';
-import 'package:bright_weddings/View/ProfileHistory/profile_history.dart';
 import 'package:bright_weddings/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:bright_weddings/Helper/get_di.dart' as di;
-
-
-import 'ChatLists/Chatlist_web.dart';
-
 import 'Helper/size_config.dart';
 
-
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   di.init();
@@ -31,25 +19,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints){
-          return OrientationBuilder(
-            builder: (BuildContext context2, Orientation orientation){
-              SizeConfig.init(constraints, orientation);
-              return GetMaterialApp(
-                theme: ThemeData(
-                  colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFFF3D48A)),
-                  primaryColor: Color(0xFFF3D48A),
-                ),
-
-                home:  ProfileEditPage(),
-
-
-
-              );
-            },
-
+        builder: (BuildContext context, BoxConstraints constraints) {
+      return OrientationBuilder(
+        builder: (BuildContext context2, Orientation orientation) {
+          SizeConfig.init(constraints, orientation);
+          return GetMaterialApp(
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFFF3D48A)),
+              primaryColor: Color(0xFFF3D48A),
+            ),
+            home: ProfileEditPage(),
           );
-        }
-    );
+        },
+      );
+    });
   }
 }
